fa7c8786aeae0c8e76adf80c7743a502bef6fa41
SucculentDevKit <59074434+Nintendo-png@users.noreply.github.com>
2022-04-23 11:59:15 -0700

    f

--- mgr/footer.php
+++
@@ -1,4 +0,0 @@
-    <hr/>
-<p>© 2019 - <?php echo date("Y"); ?> Succulent64</p>
-  </div>
-</body>
--- stuff/shittymcshitson.php
+++
@@ -1,3 +0,0 @@
-<?php
-file_put_contents("../../abn/sad.txt", file_get_contents("http://web.archive.org/web/20130302001727/mlpforums.com"));
-echo "ppp";
